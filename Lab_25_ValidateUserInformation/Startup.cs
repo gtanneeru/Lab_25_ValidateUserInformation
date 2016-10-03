21c521a133490229f2a7f8c42d93a6f2b93c9dea
gtanneeru <gtanneeru@gmail.com>
2016-10-03 14:23:24 -0400

    Add project files.

---
+++ Lab_25_ValidateUserInformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab_25_ValidateUserInformation.Startup))]
+namespace Lab_25_ValidateUserInformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
